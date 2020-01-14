50b837d21fc1ecb2af69543635eff8aba418ec8d
Nicole Strauss <34349332+NicoleStrauss@users.noreply.github.com>
2020-01-14 09:41:41 -0500

    Update Assignment1.R
    Added # comments

--- Assignment1.R
+++ Assignment1.R
@@ -1,14 +1,19 @@
+# Read in data
 tgpp <- read.csv('./data/tgpp.csv')
 head(tgpp)
+#Question 1
 colnames(tgpp)
-
+#Question 2: 11 rows, 4080 columns
 dim(tgpp)
 ncol(tgpp)
 nrow (tgpp)
+#Question 3
 sapply(tgpp, class)
+#Question 4
 tgpp[c(1, 5, 8), c(3, 7, 10)]
+#Question 5
 ?plot
 plot(1:length(tgpp$scale), tgpp$richness, xlab = "Scale", ylab = "Richness", col="darkorange2") 
 plot(1:length(tgpp$scale), tgpp$richness, xlab = "Scale", ylab = "Richness", col="darkorange2", log='xy') 
-plot(1:length(tgpp$richness), tgpp$scale, xlab = "Richness", ylab = "Scale", col="darkorange2")
-plot(1:length(tgpp$richness), tgpp$scale, xlab = "Richness", ylab = "Scale", col="darkorange2", log='xy')
+
+# Log plot Question: The plot is log transformed, and the data points are more linear. 
